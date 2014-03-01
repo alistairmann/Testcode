69cdccc7f363d8b91b734ed3b9a176b3d7dbf64c
Alistair Mann <mack.brett@gmail.com>
2014-03-01 08:34:02 +0000

    Committing base Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Superfunhappytimes::Application.config.secret_key_base = '1473ebf6c04b2a15d2562fb83588cf414d7c867db82a096bf5a6fef7b9a7a62dc2d076f4755401ca36dd978118020b63735c58826968d62408675a5937aaea03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Superfunhappytimes::Application.config.session_store :cookie_store, key: '_superfunhappytimes_session'
