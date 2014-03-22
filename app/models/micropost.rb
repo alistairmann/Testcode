098df3842722f8f51d4bf02ad5b5bf7c8a238450
Alistair Mann <alistair_123@hotmail.com>
2014-03-22 13:22:48 +0000

    CREATE USERS AND MICROPOSTS 2.3.3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ActiveRecord::Base
-end
+  validates :content, length: { maximum: 140 }
+  belongs_to :user  
+end 	
--- db/migrate/20140301104013_create_users.rb
+++ db/migrate/20140301104013_create_users.rb
@@ -1,4 +1,4 @@
-zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzclass CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
